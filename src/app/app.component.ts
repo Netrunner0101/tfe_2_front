d3c322ec72dbb417a1a8c79077cac3a42d0168b4
Eric <ericlung6572@gmail.com>
2022-11-03 04:59:03 +0100

    Add authguard and child guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import {AuthService} from "./service/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'untitled';
+
+  constructor(private auth:AuthService, private router : Router) { }
+
+  logout(){
+    this.auth.logout();
+    this.router.navigateByUrl('/');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,54 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthenticationComponent } from './component/authentication/authentication.component';
+import { CustomerComponent } from './component/customer/customer.component';
+import { DeliveryComponent } from './component/delivery/delivery.component';
+import { QuotationsComponent } from './component/quotations/quotations.component';
+import { TransporterComponent } from './component/transporter/transporter.component';
+import { WarehouseComponent } from './component/warehouse/warehouse.component';
+import {RouterLinkWithHref, RouterModule, RouterOutlet, Routes} from "@angular/router";
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+import {HTTP_INTERCEPTORS, HttpClientModule, HttpInterceptor} from "@angular/common/http";
+import {TokenInterceptorInterceptor} from "./token-interceptor.interceptor";
+import {AuthguardGuard} from "./component/authentication/authguard.guard";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationComponent,
+    CustomerComponent,
+    DeliveryComponent,
+    QuotationsComponent,
+    TransporterComponent,
+    WarehouseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterOutlet,
+    HttpClientModule,
+    NoopAnimationsModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterLinkWithHref,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS,useClass:TokenInterceptorInterceptor, multi:true},
+    AuthguardGuard,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
